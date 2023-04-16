0ff3084cd22ac72ea3c176c7f38ad5422982ed21
Albert I Palacios <albertpalacios70@yahoo.com>
2023-04-16 20:28:07 +0000

    Integrated one function to take the output of the other

--- passwd_generator.py
+++ passwd_generator.py
@@ -9,25 +9,27 @@ alphas = letters + digits + special_chars
 def pwd_length():
   while True:
     try:
-      pwd_length = int(input("Choose your password length: "))
-      if pwd_length <= 5:
+      length = int(input("Choose your password length: "))
+      if length <= 5:
         print("The length must exceed 5 characters. Please try again.")
         continue
     except ValueError:
       print("This is not a valid entry. You must only enter a number.")
     else:
       break
-pwd_length()
+  return length
 
-#def secrets_generator(pwd_length):
-#  while True:
-#    pwd = ''
-#    for i in pwd_length:
-#      pwd += ''.join(secrets.choice(alphas))
+length = pwd_length()
 
-#    if (any(char in special_chars for char in pwd) and 
-#        sum(char in digits for char in pwd)>=2):
-#          break
-#  print(pwd)
+def secrets_generator(length):
+  while True:
+    pwd = ''
+    for i in range(length):
+      pwd += ''.join(secrets.choice(alphas))
 
-#secrets_generator(pwd_length)
+    if (any(char in special_chars for char in pwd) and 
+        sum(char in digits for char in pwd)>=2):
+          break
+  print(pwd)
+
+secrets_generator(length)
