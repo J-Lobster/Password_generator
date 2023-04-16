b0519061526bf44d139eee6ddca9190d7c724712
Albert I Palacios <albertpalacios70@yahoo.com>
2023-04-16 18:04:23 +0000

    attempting to integrate two functions together

--- passwd_generator.py
+++ passwd_generator.py
@@ -1,5 +1,4 @@
-import secrets
-import string
+import secrets, string
 
 letters = string.ascii_letters
 digits = string.digits
@@ -15,8 +14,20 @@ def pwd_length():
         print("The length must exceed 5 characters. Please try again.")
         continue
     except ValueError:
-      print("This is not a valid entry. You may enter a number only.")
+      print("This is not a valid entry. You must only enter a number.")
     else:
       break
-
 pwd_length()
+
+#def secrets_generator(pwd_length):
+#  while True:
+#    pwd = ''
+#    for i in pwd_length:
+#      pwd += ''.join(secrets.choice(alphas))
+
+#    if (any(char in special_chars for char in pwd) and 
+#        sum(char in digits for char in pwd)>=2):
+#          break
+#  print(pwd)
+
+#secrets_generator(pwd_length)
