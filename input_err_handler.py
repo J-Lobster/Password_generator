8bfaeb145ac8f28d8a3828b6c93769664916e614
Albert I Palacios <albertpalacios70@yahoo.com>
2023-04-30 16:01:35 +0000

    creating a new function to handle the hang ups

---
+++ input_err_handler.py
@@ -0,0 +1,89 @@
+import secrets, string
+import array as arr
+
+letters = string.ascii_letters
+digits = string.digits
+special_chars = string.punctuation
+
+alpha = letters + digits + special_chars
+
+def usr_pwd_length():
+  while True:
+    try:
+      len = int(input("Choose your password length: "))
+      if len <= 7:
+        print("The length must exceed 8 characters. Please try again.")
+        continue
+      elif len >= 21:
+        print("You may not exceed a length of 21. Please try again.")
+        continue
+    except ValueError:
+      print("This is not a valid entry. Only numbers are allowed.")
+    else:
+      break
+  return len
+
+length = usr_pwd_length()
+
+def usr_digits_length():
+  while True:
+    try:
+      min_digits = int(input("Enter a number of digits you wish to use: "))
+      if min_digits <= 1:
+        print(f"{min_digits} does not meet the minimum requirement. Please try again.")
+        continue
+      elif min_digits >= 9:
+        print (f"{min_digits} exceeds the maximum number of digits permitted. Please try again.")
+        continue
+    except ValueError:
+      print("This is an invalid entry. Only numbers are allowed.")
+      continue
+    else:
+      break
+  return min_digits
+
+digits_input = usr_digits_length()
+
+def usr_specials_length():
+  while True:
+    try:
+      min_special_chars = int(input("Enter a number of special characters you wish to use: "))
+      if min_special_chars <= 1:
+        print(f"{min_special_chars} does not meet the minimum requirement. Please try again.")
+        continue
+      elif min_special_chars >= 9:
+        print(f"{min_special_chars} exceeds the maximum number of special characters permitted. Please try again.")
+        continue
+    except ValueError:
+      print("This is an invalid entry. Only numbers are allowed.")
+      continue
+    else:
+      break
+  return min_special_chars
+
+specials_input = usr_specials_length()
+
+def input_err_handler(length):
+  while length == 8:
+    if digits_input and specials_input > 4:
+      print("nah nigga, do it again")
+      usr_digits_length() and usr_specials_length()
+      continue
+    else:
+      break
+  return input_err_handler
+
+input_err_handler = input_err_handler(length)
+
+def generator():
+  while True:
+    pwd = ''
+    for i in range(length):
+      pwd += ''.join(secrets.choice(alpha))
+
+    if (sum(char in special_chars for char in pwd) == specials_input and 
+        sum(char in digits for char in pwd) == digits_input):
+          break
+  print(pwd)
+
+generator()
