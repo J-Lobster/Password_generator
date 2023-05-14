b1c1bc42459e9697de40754f483b7abbd04027df
Albert I Palacios <albertpalacios70@yahoo.com>
2023-05-14 19:56:34 +0000

    Code is in a functioning state but requires a ton of refactoring

--- passwd_generator.py
+++ passwd_generator.py
@@ -1,174 +1,261 @@
 import secrets, string
+from english_words import get_english_words_set
 
-letters = string.ascii_letters
-digits = string.digits
-special_chars = string.punctuation
-
-alpha = letters + digits + special_chars
-
-def pwd_length():
+def length_input():
   while True:
     try:
-      len = int(input("Choose your password length: "))
-      if len <= 7:
-        print("The length must exceed 8 characters. Please try again.")
+      length = int(input("Choose your password length: "))
+      if length < 8:
+        print("The length must exceed 7. Please try again.")
         continue
-      elif len >= 21:
-        print("You may not exceed a length of 21. Please try again.")
+      elif length > 20:
+        print("You cannot not exceed a length of 20. Please try again.")
         continue
     except ValueError:
       print("This is not a valid entry. Only numbers are allowed.")
     else:
       break
-  return len
+  return length
 
-length = pwd_length()
-
-def digit_count():
+def digits_input():
   while True:
     try:
-      min_digits = int(input("Enter a number of digits you wish to use: "))
-      if min_digits <= 0:
-        print(f"{min_digits} does not meet the minimum requirement. Please try again.")
+      digits = int(input("Enter a number of digits you wish to use: "))
+      if digits < 1:
+        print(f"{digits} does not meet the minimum requirement. Please try again.")
         continue
-      elif min_digits >= 9:
-        print (f"{min_digits} exceeds the maximum number of digits permitted. Please try again.")
+      elif digits > 8:
+        print (f"{digits} exceeds the maximum number of digits permitted. Please try again.")
         continue
     except ValueError:
       print("This is an invalid entry. Only numbers are allowed.")
       continue
     else:
       break
-  return min_digits
+  return digits
 
-def specials_count():
+
+
+def specials_input():
   while True:
     try:
-      8
-      min_special_chars = int(input("Enter a number of special characters you wish to use: "))
-      if min_special_chars <= 0:
-        print(f"{min_special_chars} does not meet the minimum requirement. Please try again.")
+      specials = int(input("Enter a number of special characters you wish to use: "))
+      if specials < 1:
+        print(f"{specials} does not meet the minimum requirement. Please try again.")
         continue
-      elif min_special_chars >= 9:
-        print(f"{min_special_chars} exceeds the maximum number of special characters permitted. Please try again.")
+      elif specials > 8:
+        print(f"{specials} exceeds the maximum number of special characters permitted. Please try again.")
         continue
     except ValueError:
       print("This is an invalid entry. Only numbers are allowed.")
       continue
     else:
       break
-  return min_special_chars
+  return specials
+
+def letters_input():  
+  while length == 8:   
+    try:
+      letters = int(input("Choose the number of letters you desire in your word: "))
+      if letters <= 0:
+        print(f"{letters} is not a valid entry. Please select a higher letter count.")
+        continue
+      elif letters >= 4:
+        print(f"{letters} is too many letters. Choose a number lower than 5")
+        continue
+    except ValueError:
+        print("This is not a valid entry. Only numbers are allowed.")  
+    else:
+      break
+
+  while length == 9:   
+    try:
+      letters = int(input("Choose the number of letters you desire in your word: "))
+      if letters <= 0:
+        print(f"{letters} is not a valid entry. Please select a higher letter count.")
+        continue
+      elif letters >= 5:
+        print(f"{letters} is too many letters. Choose a number lower than 5")
+        continue
+    except ValueError:
+        print("This is not a valid entry. Only numbers are allowed.")  
+    else:
+      break
+
+  while length == 10:   
+    try:
+      letters = int(input("Choose the number of letters you desire in your word: "))
+      if letters <= 0:
+        print(f"{letters} is not a valid entry. Please select a higher letter count.")
+        continue
+      elif letters >= 6:
+        print(f"{letters} is too many letters. Choose a number lower than 5")
+        continue
+    except ValueError:
+        print("This is not a valid entry. Only numbers are allowed.")  
+    else:
+      break  
+
+  while length == 11:   
+    try:
+      word_input = int(input("Choose the number of letters you desire in your word: "))
+      if letters <= 0:
+        print(f"{letters} is not a valid entry. Please select a higher letter count.")
+        continue
+      elif letters >= 7:
+        print(f"{letters} is too many letters. Choose a number lower than 5")
+        continue
+    except ValueError:
+        print("This is not a valid entry. Only numbers are allowed.")  
+    else:
+      break  
+  
+  while length == 12:   
+    try:
+      letters = int(input("Choose the number of letters you desire in your word: "))
+      if letters <= 0:
+        print(f"{letters} is not a valid entry. Please select a higher letter count.")
+        continue
+      elif letters >= 8:
+        print(f"{letters} is too many letters. Choose a number lower than 5")
+        continue
+    except ValueError:
+        print("This is not a valid entry. Only numbers are allowed.")  
+    else:
+      break
+  return letters
+
+def word_gen():
+  letters = letters_input()
+  word_list = list(get_english_words_set(['web2'], alpha=True))
+  for word in word_list:
+    if len(word) > letters:
+      continue
+    else:
+      if len(word) == letters:
+        print(word)
+        break
+  return word
 
 def input_handler():
-    digits_input = digit_count()
-    specials_input = specials_count()
-    while length == 8:
+    input_d = digits_input()
+    input_s = specials_input()
+    inputs = input_d + input_s
+    while word == 3:
       try:
-        if digits_input and specials_input >= 5:
-          print(f"You entered {digits_input} for both inputs. Your inputs cannot be exceed your password length.")
-          digits_input = digit_count()
-          specials_input = specials_count()
-          continue
+        if inputs == 5:
+          break
       except ValueError:
         print("This is not a valid entry. Only numbers are allowed.")
       else:
-        break
-
-    while length == 9:
-      try:
-        if digits_input and specials_input >= 5:
-          print(f"You entered {digits_input} for both inputs. Your inputs cannot be exceed your password length.")
-          digits_input = digit_count()
-          specials_input = specials_count()
+          print(f"You entered {inputs} for both inputs. Your inputs cannot be exceed your password length.")
+          input_d = digits_input()
+          input_s = specials_input()
           continue
+
+    while word == 3:
+      try:
+        if inputs == 5:
+          break
       except ValueError:
         print("This is not a valid entry. Only numbers are allowed.")
       else:
-        break
-
-    while length == 10:
-      try:
-        if digits_input and specials_input >= 6:
-          print(f"You entered {digits_input} for both inputs. Your inputs cannot be exceed your password length.")
-          digits_input = digit_count()
-          specials_input = specials_count()
+          print(f"You entered {inputs} for both inputs. Your inputs cannot be exceed your password length.")
+          input_d = digits_input()
+          input_s = specials_input()
           continue
+
+    while word == 3:
+      try:
+        if inputs == 5:
+          break
       except ValueError:
         print("This is not a valid entry. Only numbers are allowed.")
       else:
-        break
-
-    while length == 11:
-      try:
-        if digits_input and specials_input >= 6:
-          print(f"You entered {digits_input} for both inputs. Your inputs cannot be exceed your password length.")
-          digits_input = digit_count()
-          specials_input = specials_count()
+          print(f"You entered {inputs} for both inputs. Your inputs cannot be exceed your password length.")
+          input_d = digits_input()
+          input_s = specials_input()
           continue
+
+    while word == 3:
+      try:
+        if inputs == 5:
+          break
       except ValueError:
         print("This is not a valid entry. Only numbers are allowed.")
       else:
-        break
-
-    while length == 12:
-      try:
-        if digits_input and specials_input >= 7:
-          print(f"You entered {digits_input} for both inputs. Your inputs cannot be exceed your password length.")
-          digits_input = digit_count()
-          specials_input = specials_count()
+          print(f"You entered {inputs} for both inputs. Your inputs cannot be exceed your password length.")
+          input_d = digits_input()
+          input_s = specials_input()
           continue
+
+    while word == 3:
+      try:
+        if inputs == 5:
+          break
       except ValueError:
         print("This is not a valid entry. Only numbers are allowed.")
       else:
-        break
-
-    while length == 13:
-      try:
-        if digits_input and specials_input >= 7:
-          print(f"You entered {digits_input} for both inputs. Your inputs cannot be exceed your password length.")
-          digits_input = digit_count()
-          specials_input = specials_count()
+          print(f"You entered {inputs} for both inputs. Your inputs cannot be exceed your password length.")
+          input_d = digits_input()
+          input_s = specials_input()
           continue
+
+    while word == 3:
+      try:
+        if inputs == 5:
+          break
       except ValueError:
         print("This is not a valid entry. Only numbers are allowed.")
       else:
-        break
-
-    while length == 14:
-      try:
-        if digits_input and specials_input >= 8:
-          print(f"You entered {digits_input} for both inputs. Your inputs cannot be exceed your password length.")
-          digits_input = digit_count()
-          specials_input = specials_count()
+          print(f"You entered {inputs} for both inputs. Your inputs cannot be exceed your password length.")
+          input_d = digits_input()
+          input_s = specials_input()
           continue
+
+    while word == 3:
+      try:
+        if inputs == 5:
+          break
       except ValueError:
         print("This is not a valid entry. Only numbers are allowed.")
       else:
-        break
-
-    while length == 15:
-      try:
-        if digits_input and specials_input >= 8:
-          print(f"You entered {digits_input} for both inputs. Your inputs cannot be exceed your password length.")
-          digits_input = digit_count()
-          specials_input = specials_count() 
+          print(f"You entered {inputs} for both inputs. Your inputs cannot be exceed your password length.")
+          input_d = digits_input()
+          input_s = specials_input()
           continue
+
+    while word == 3:
+      try:
+        if inputs == 5:
+          break
       except ValueError:
         print("This is not a valid entry. Only numbers are allowed.")
       else:
-        break
-    return digits_input, specials_input
+          print(f"You entered {inputs} for both inputs. Your inputs cannot be exceed your password length.")
+          input_d = digits_input()
+          input_s = specials_input()
+          continue
+    return input_d, input_s
 
-def generator():
+
+
+def password_generator():
   digit, special = input_handler()
   while True:
     pwd = ''
     for i in range(length):
-      pwd += ''.join(secrets.choice(alpha))
+      pwd += ''.join(secrets.choice(word + non_alpha))
 
     if (sum(char in special_chars for char in pwd) == special and 
         sum(char in digits for char in pwd) == digit):
           break
   print(pwd)
 
-generator()
+
+special_chars = string.punctuation
+digits = string.digits
+non_alpha = digits + special_chars
+length = length_input()
+word = word_gen()
+password = password_generator()
