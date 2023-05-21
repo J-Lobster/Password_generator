f614a309c70f245800571332b00afa4286c3c9ab
Albert I Palacios <albertpalacios70@yahoo.com>
2023-05-21 20:34:24 +0000

    included error handling in the case of digits and/or special characters exceeding the letter count for the word included in the password.

--- revised_pwd_gen.py
+++ revised_pwd_gen.py
@@ -1,10 +1,6 @@
 import secrets, string 
 from english_words import get_english_words_set
 
-numbers = string.digits
-special_chars = string.punctuation
-non_alpha = numbers + special_chars
-
 def usr_inputs():
   max_letters_limit = {
     8: 3,
@@ -22,6 +18,23 @@ def usr_inputs():
     20: 15
   }
 
+  max_others_limit = {
+    2: [(3, 3), (2, 4), (4, 2), (1, 5), (5, 1)],
+    3: [(1, 4), (4, 1), (2, 3), (3, 2)],
+    4: [(1, 4), (4, 1), (2, 3), (3, 2)],
+    5: [(1, 4), (4, 1), (2, 3), (3, 2)],
+    6: [(1, 4), (4, 1), (2, 3), (3, 2)],
+    7: [(1, 4), (4, 1), (2, 3), (3, 2)],
+    8: [(1, 4), (4, 1), (2, 3), (3, 2)],
+    9: [(1, 4), (4, 1), (2, 3), (3, 2)],
+    10: [(1, 4), (4, 1), (2, 3), (3, 2)],
+    11: [(1, 4), (4, 1), (2, 3), (3, 2)],
+    12: [(1, 4), (4, 1), (2, 3), (3, 2)],
+    13: [(1, 4), (4, 1), (2, 3), (3, 2)],
+    14: [(1, 4), (4, 1), (2, 3), (3, 2)],
+    15: [(1, 4), (4, 1), (2, 3), (3, 2)]
+  }
+
   while True:
     try:
       length = int(input("Choose your password length: "))
@@ -32,49 +45,37 @@ def usr_inputs():
       print("This is not a valid entry. Only numbers are allowed.")
     else:
       break
-
+    
   while True:
     try:
       letters = int(input(f"Choose the number of letters you desire in your word (up to {max_letters_limit[length]}): "))
-      if letters <= 0 or letters > max_letters_limit[length]:
+      if letters <= 1 or letters > max_letters_limit[length]:
         print(f"Invalid entry. Choose a number up to {max_letters_limit[length]}.")
         continue
     except ValueError:
       print("Invalid entry. Only numbers are allowed.")
     else:
-      break  
+      break
 
   while True:
     try:
       digits = int(input("Enter a number of digits you wish to use: "))
-      if digits < 1 or digits > 8:
-        print(f"{digits} does not meet the requirements. Please select from a range of 1-8.")
-        continue
-    except ValueError:
-      print("This is an invalid entry. Only numbers are allowed.")
-    else:
-      break
-
-  while True:
-    try:
       specials = int(input("Enter a number of special characters you wish to use: "))
-      if specials < 1 or specials > 8:
-        print(f"{specials} does not meet the requirements. Please select from a range of 1-8.")
+      if digits <= 0 or specials <= 0 or (digits, specials) not in max_others_limit[letters]:
+        print(f"One or both of your choices: ({digits}, {specials}) exceeds the allowed values for either category. \nYou can choose from the following options: {max_others_limit[letters]}")
         continue
     except ValueError:
       print("This is an invalid entry. Only numbers are allowed.")
     else:
       break
-  return length, digits, specials, letters
 
-length, digits, specials, letters = usr_inputs()
-
-#def input_handler(digits, specials, letters):
-
-
-
+  return length, letters, digits, specials
 
 def password_generator():
+  numbers = string.digits
+  special_chars = string.punctuation
+  non_alpha = numbers + special_chars
+  length, letters, digits, specials = usr_inputs()
   word_list = list(get_english_words_set(['web2'], alpha=True))
   for word in word_list:
     if len(word) > letters:
