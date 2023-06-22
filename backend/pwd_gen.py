20aff7411b1bdf6ba03ace0fd6a63bf3c9c78266
Albert I Palacios <albertpalacios70@yahoo.com>
2023-06-22 17:00:34 +0000

    allowed custom word input but struggling to restrict white space use as an input.

--- backend/pwd_gen.py
+++ backend/pwd_gen.py
@@ -1,11 +1,9 @@
 import secrets, string 
-from english_words import get_english_words_set
 from tables import alpha_tables, non_alpha_tables
 
 
 def usr_inputs():
   max_letters_limit = alpha_tables()
-
   max_nonalpha_limit = non_alpha_tables()
 
   while True:
@@ -21,9 +19,13 @@ def usr_inputs():
     
   while True:
     try:
-      letters = int(input(f"Enter the number of letters you wish to use (up to {max_letters_limit.max_letters[length]}): "))
-      if letters <= 1 or letters > max_letters_limit.max_letters[length]:
-        print(f"Invalid entry. Choose a number up to {max_letters_limit.max_letters[length]}.")
+      blank = ''
+      letters = str(input(f"Enter a word that are up to {max_letters_limit.max_letters[length]} letters long: "))
+      if len(letters) <= 1 or len(letters) > max_letters_limit.max_letters[length]:
+        print(f"Invalid entry. Create a word up to {max_letters_limit.max_letters[length]} letters long.")
+        continue
+      if blank in letters:
+        print("You did not enter any letters, please enter a word.")
         continue
     except ValueError:
       print("Invalid entry. Only numbers are allowed.")
@@ -34,8 +36,8 @@ def usr_inputs():
     try:
       digits = int(input("Enter a number of digits you wish to use: "))
       specials = int(input("Enter a number of special characters you wish to use: "))
-      if digits <= 0 or specials <= 0 or (digits, specials) not in max_nonalpha_limit.get_nonalpha(length).get(letters, []):
-        print(f"One or both of your choices is an invalid entry. \nDigits: {digits} \nSpecials: {specials} \nPlease select from the following {max_nonalpha_limit.get_nonalpha(length).get(letters, [])}")
+      if digits <= 0 or specials <= 0 or (digits, specials) not in max_nonalpha_limit.get_nonalpha(length).get(len(letters), []):
+        print(f"One or both of your choices is an invalid entry. \nDigits: {digits} \nSpecials: {specials} \nPlease select from the following {max_nonalpha_limit.get_nonalpha(length).get(len(letters), [])}")
         continue
     except ValueError:
       print("This is an invalid entry. Only numbers are allowed.")
@@ -47,22 +49,14 @@ def usr_inputs():
 def password_generator():
   numbers = string.digits
   special_chars = string.punctuation
+  alpha = string.ascii_letters
   non_alpha = numbers + special_chars
   length, letters, digits, specials = usr_inputs()
-  word_list = list(get_english_words_set(['web2'], alpha=True))
-
-  for word in word_list:
-    if len(word) > letters:
-      continue
-    else:
-      if len(word) == letters:
-        print(word)
-        break
 
   while True:
-    pwd = ''
-    for i in range(length):
-      pwd += ''.join(secrets.choice(word + non_alpha))
+    pwd = letters
+    for i in range(length - len(letters)):
+      pwd += ''.join(secrets.choice(non_alpha))
 
     if (sum(char in special_chars for char in pwd) == specials and 
         sum(char in numbers for char in pwd) == digits):
