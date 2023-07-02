b8a14d72ec68d21b2904ad1d3a87b6ea197851e8
Albert I Palacios <albertpalacios70@yahoo.com>
2023-07-02 16:10:15 +0000

    Attempting to modify code in order for it to pass params on the web page

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,22 +1,22 @@
 import secrets, string, random
 from pwd_gen import usr_inputs, generator
-from tables import alpha_tables, non_alpha_tables
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    inputs = usr_inputs
-    return render_template('index.html')
+    if request.method == "POST":
+        length = int(request.form['length'])
+        letters = (request.form['letters'])
+        digits = int(request.form['digits'])
+        specials = int(request.form['specials'])
 
-@app.route('/generate', methods=['POST'])
-def generated_password():
-    length = request.form.get('length')
-    letters = request.form.get('letters')
-    digits = request.form.get('digits')
-    specials = request.form.get('specials')
-    return render_template('generator.html', generated_password=generated_password)
+        password = generator(length, letters, digits, specials)
+
+        return render_template('index.html', password=password)
+        
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
