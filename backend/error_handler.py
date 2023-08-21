1fc791e23578d167856fb919823f200b3b7f9072
Albert I Palacios <albertpalacios70@yahoo.com>
2023-08-21 23:27:39 +0000

    small change to error_handler.py

--- backend/error_handler.py
+++ backend/error_handler.py
@@ -10,8 +10,8 @@ def error_handler(length, letters, digits, specials):
   elif len(letters) <= 1 or len(letters) > max_alpha_limit.get_alpha_table[length]:
     error_messages.append(f"{letters} is {len(letters)} letters long. Create a word up to {max_alpha_limit.get_alpha_table[length]} letters long.")
   elif (digits, specials) not in max_nonalpha_limit.get_nonalpha_table(length).get(len(letters)):
-    error_messages.append(f"Digit: {digits}")
-    error_messages.append(f"Special character: {specials}")
+    error_messages.append(f"Digit(s): {digits}")
+    error_messages.append(f"Special character(s): {specials}")
     error_messages.append(f"One or both of your choices is an invalid entry.")
     error_messages.append(f"Choose from the following combinations: {max_nonalpha_limit.get_nonalpha_table(length).get(len(letters))}")
   return error_messages
